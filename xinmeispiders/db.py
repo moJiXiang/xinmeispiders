6fa1a4cd520da7bdc68b51be51ccd050a8d105c5
moJiXiang <mojixiang1102@gmail.com>
2015-05-25 17:18:28 +0800

    修改关键词数据库来源

--- xinmeispiders/db.py
+++ xinmeispiders/db.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
 from pymongo import MongoClient
 # 链接数据库
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://xinmei:xinmei@117.121.25.124:27017/xinmei-development")
 db = client["xinmei-development"]
